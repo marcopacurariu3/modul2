b40522cba6defe324af922cf24c0ccd0b3d1c54c
marcopacurariu3 <71716843+marcopacurariu3@users.noreply.github.com>
2025-01-30 19:14:57 +0200

    First commit with project structure (spring web, spring data & postgresql dependencies)

---
+++ src/test/java/com/modul2/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modul2.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
